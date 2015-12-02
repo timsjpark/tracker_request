72d860af23615dcd0c7cd3bcb9a704087f40d7dc
Daniel Debevec <daniel.debevec@gmail.com>
2015-12-01 19:15:19 -0700

    Require login to access console
    [#108928288]

--- app/controllers/console_controller.rb
+++ app/controllers/console_controller.rb
@@ -1,4 +1,6 @@
 class ConsoleController < ApplicationController
+  before_filter :login_required
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 
   get 'console/index'
-
   get 'console/profile'
-
   get 'console/statistics'
 
   # The priority is based upon order of creation: first created -> highest priority.
