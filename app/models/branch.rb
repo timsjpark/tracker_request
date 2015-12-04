fc099ef886419eacccfeac5e0ae8508b4c9e81c9
Tim Park <timsjpark@gmail.com>
2015-12-03 20:21:49 -0700

    Set up all has_many associations

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,3 +1,4 @@
 class Branch < ActiveRecord::Base
   belongs_to :repository
+  has_many :commits
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :repositories
+
   def decrypt_key(encrypted_key)
     Encrypt_Decrypt.new.decrypt(encrypted_key)
   end
