3bb36e19b4e74cfb991df8897ddb62e1bd1e7344
Tim Park <timsjpark@gmail.com>
2015-12-10 17:01:14 -0700

    Create StoryImportWorker

--- app/workers/story_import_worker.rb
+++ app/workers/story_import_worker.rb
@@ -1,44 +1,41 @@
-class BranchImportWorker
-
-  @queue = :branch_import_worker
+class StoryImportWorker
+  @queue = :story_import_worker
 
   def self.perform(current_user_id)
-    #RepoImport.new(current_user).repo_api_call
     current_user = User.find(current_user_id)
     @client = Client.new(current_user)
-    @client_connect = @client.github_client
+    @client_connect = @client.pivotal_client
 
-    Repository.where(user_id: current_user_id).find_each do |repo_info|
+    Project.where(user_id: current_user_id).find_each do |project_info|
 
-      @branch_info = @client_connect.branches("#{repo_info[:repo_full_name]}")
-      @branch_info.each do |branch_info|
-        branch = Branch.where(latest_commit_sha: branch_info[:commit][:sha]).first_or_initialize
+      @project = @client_connect.project(project_info[:id])
+      @stories = @project.stories()
+      @stories.each do |story_info|
+        story = Story.where(story_ident: story_info[:id]).first_or_initialize
 
-        branch.update(branch_params(branch_info,repo_info))
+        story.update(story_params(story_info, project_info))
       end
+      # @branch_info = @client_connect.branches("#{repo_info[:repo_full_name]}")
+      # @branch_info.each do |branch_info|
+      #   branch = Branch.where(latest_commit_sha: branch_info[:commit][:sha]).first_or_initialize
+      #
+      #   branch.update(branch_params(branch_info,repo_info))
+      # end
     end
-    Resque.enqueue_in(65.seconds, BranchImportWorker, current_user_id)
+
+    Resque.enqueue_in(65.seconds, StoryImportWorker, current_user_id)
   end
 
-  def self.branch_params(branch_info,repo_info)
+  def self.story_params(story_info, project_info)
     {
-        branch_name: branch_info[:name],
-        branch_github_ident: branch_info[:id],
-        latest_commit_sha: branch_info[:commit][:sha],
-        repository_id: repo_info[:id]
+      story_ident: story_info[:id],
+      kind: story_info[:kind],
+      name: story_info[:name],
+      description: story_info[:description],
+      story_type: story_info[:story_type],
+      current_state: story_info[:current_state],
+      estimate: story_info[:estimate],
+      project_id: story_info[:project_id]
     }
-  end
-
-  create_table "stories", force: :cascade do |t|
-    t.integer  "story_ident"
-    t.string   "kind"
-    t.string   "name"
-    t.text     "description"
-    t.string   "story_type"
-    t.string   "current_state"
-    t.string   "estimate"
-    t.integer  "project_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
   end
 end
