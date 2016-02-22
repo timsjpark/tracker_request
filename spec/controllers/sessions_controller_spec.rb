a84bd844e3df35fbcab7a50dc829805685639f02
Matt Michnal <mattm3646@gmail.com>
2016-02-21 18:28:40 -0700

    Added first set of RSpec tests and refactoring

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,9 +1,2 @@
 require 'rails_helper'
 
-RSpec.describe ConsoleController, type: :controller do
-  # Testing in this controller needs to make sure that
-  # a session can be created, destroyed, and the user
-  # is properly added to the database.
-end
-
-
