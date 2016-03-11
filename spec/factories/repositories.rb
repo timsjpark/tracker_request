31d8ffe90087dd8fbe0647e59e68b601dc3de82f
Matt Michnal <mattm3646@gmail.com>
2016-03-11 09:56:34 -0700

    Modified factory for repos in spec

--- spec/factories/repositories.rb
+++ spec/factories/repositories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   # Add the user association in the Factories so that association can be tested properly
   factory :repository do
-  id { Faker::Number.between(1, 20) }
+  id { Faker::Number }
   repo_name { Faker::Lorem.word }
   number_of_forks { Faker::Number.between(1, 6) }
   forked false
