c4fb3787e6b85af8a866ba634ea6fc896afd7c24
Matt Michnal <mattm3646@gmail.com>
2016-03-30 11:08:29 -0600

    Fixed console spec

--- spec/controllers/console_controller_spec.rb
+++ spec/controllers/console_controller_spec.rb
@@ -31,30 +31,26 @@ RSpec.describe ConsoleController, type: :controller do
     before do
       OmniAuthMock::User.new.current_user
     end
-    before do
-      Capybara.default_host = 'http://127.0.0.1'
-      puts "Capybara.current_host: #{Capybara.current_host}"
-      puts "Capybara.default_host: #{Capybara.default_host}"
-      puts "Capybara.current_url: #{Capybara.current_url}"
-    end
+
     describe "User tries to access the console page" do
       it "allows user to view console#index page" do
-          Capybara.default_host = 'http://127.0.0.1:3000'
-          capy = Cabybara.default_host
-          visit '/'
-          visit "#{capy}/auth/github"
-          save_and_open_page
-          #click_link('Signin')
-          visit 'auth/github/callback'
-          expect(page).to have_content('Sign In!')
+        visit '/console'
+        expect(response).to have_http_status(:success)
       end
     end
 
     describe "User tries to access profile page" do
-      it "allows user to view console#profile page"
+      it "allows user to view console#profile page" do
+        visit '/console/profile'
+        expect(response).to have_http_status(:success)
+      end
     end
+
     describe "User tries to access the statistics page" do
-      it "allows user to view console#statistics page"
+      it "allows user to view console#statistics page" do
+        visit '/console/statistics'
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
