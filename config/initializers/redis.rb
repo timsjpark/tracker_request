bb5915ab43c7f898907b3f9f33d149afc2cd3ae8
Matt Michnal <mattm3646@gmail.com>
2016-03-31 17:19:34 -0600

    Trying REDIS_URL to get the redis database to work

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 ENV['REDISTOGO_URL'] ||= "redis://username:password@host:1234/"
 
-uri = URI.parse(ENV['REDISTOGO_URL']) 
+uri = URI.parse(ENV['REDIS_URL']) 
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
