934969a9121bbbb168aef5915b646e46674b88f3
Matt Michnal <mattm3646@gmail.com>
2016-03-31 18:33:34 -0600

    Fixed error in syntax for initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 
-uri = URI.parse(ENV["REDISTOGO_URL")
+uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Resque.redis = REDIS
 
