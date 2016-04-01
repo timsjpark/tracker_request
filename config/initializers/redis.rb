02bd3e634a58e24a2a1a81d6dd6b1dd5e32ddfe6
Matt Michnal <mattm3646@gmail.com>
2016-04-01 12:12:52 -0600

    changed redis to match Rails 4.2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,8 +4,8 @@ if ENV['REDISTOGO_URL'].blank?
   REDIS = uri
 else
   uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  REDIS = Redis.new(:url => uri)
 end
 
-Resque.redis = REDIS 
+Resque.redis = REDIS
 Dir["/app/app/jobs/*.rb"].each { |file| require file }
