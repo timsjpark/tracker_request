a88ab731cd235d3078c1f575a441897bb68bb422
Matt Michnal <mattm3646@gmail.com>
2016-04-01 11:05:32 -0600

    Trying to directly connect using Redis.connect

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Resque.redis = REDIS
+#uri = URI.parse(ENV["REDISTOGO_URL"])
+#REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
+Resque.redis = Redis.connect(:url => ENV['REDISTOGO_URL'])
 Dir["/app/app/jobs/*.rb"].each { |file| require file }
