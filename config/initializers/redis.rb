2e0826654dabbaa6cfb0005f21b27fd145634c12
Matt Michnal <mattm3646@gmail.com>
2016-03-31 18:25:00 -0600

    Made Resque.redis recognize the prod Redis DB

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 
 uri = URI.parse(ENV.fetch("REDISTOGO_URL", "redis://localhost:6379/"))
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS
 
 Dir["/app/app/jobs/*.rb"].each { |file| require file }
