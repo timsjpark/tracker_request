77b9a099a88e1f2205635bf2fea0636dcf946442
Matt Michnal <mattm3646@gmail.com>
2016-04-01 12:04:28 -0600

    Still working on Redis initialize issue

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,11 @@
 
-#uri = URI.parse(ENV["REDISTOGO_URL"])
-#REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if ENV['REDISTOGO_URL'].blank?
+  uri = '127.0.0.1:6379'
+  REDIS = uri
+else
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
 
-Resque.redis = Redis.connect(:url => ENV['REDISTOGO_URL'])
+Resque.redis = REDIS 
 Dir["/app/app/jobs/*.rb"].each { |file| require file }
