f77a0c7ea9c3af365045a11f7f96a541bd5c8933
Matt Michnal <mattm3646@gmail.com>
2016-04-20 00:18:59 -0600

    Added Active Record destroy methods to DB updater

--- lib/github_api/database_updater.rb
+++ lib/github_api/database_updater.rb
@@ -36,16 +36,15 @@ class GithubApi::Database_Updater
     repositories = Repository.where(user_id: current_user.id).all
     repo_ids = []
     repositories.each {|x| repo_ids << x.repo_github_ident}
+    Repository.where(repo_github_ident: repo_ids - api_ids).destroy_all
     repo_ids.each {|x| puts x}
-    puts "==============="
-    api_ids.each {|x| puts x}
-    puts "This is the repo that needs to be deleted #{repo_ids - api_ids}"
   end
 
   def branch_matcher(current_user, api_ids, repo_ids)
     branches = Branch.where(repository_id: repo_ids).select([:latest_commit_sha, :branch_name])
     branch_commit_sha = []
     branches.each {|x| branch_commit_sha << "#{x.branch_name} #{x.latest_commit_sha}"}
+    #Branch.where(id: branch_commit_sha - api_ids).destroy_all
     branch_commit_sha.each {|x| puts x}
     puts "=============="
     api_ids.each {|x| puts x}
@@ -58,12 +57,7 @@ class GithubApi::Database_Updater
     pull_requests = PullRequest.where(repository_id: repo_ids).select([:pr_github_ident])
     pr_github_array = []
     pull_requests.each {|x| pr_github_array << x.pr_github_ident}
-    pr_github_array.each {|x| puts x}
-    puts "=========="
-    api_ids.each {|x| puts x}
-    puts "Branch Model counts #{pr_github_array.count}"
-    puts "Branch API counts #{api_ids.count}"
-    puts "These are the branches that needs to be deleted #{pr_github_array - api_ids}"
+    PullRequest.where(pr_github_ident: pr_github_array - api_ids).destroy_all
   end
 
   def pull_request_comment_matcher(current_user, api_ids, repo_ids)
--- lib/github_api/pull_request_import.rb
+++ lib/github_api/pull_request_import.rb
@@ -7,14 +7,19 @@ class GithubApi::PullRequestImport
   end
 
   def pull_requests_to_db
+    repo_id_array = []
+    pull_request_api_array = []
     Repository.where(user_id: @current_user.id).find_each do |repo_info|
+      repo_id_array << repo_info.id
       @pull_request_info = @client_connect.pull_requests("#{repo_info[:repo_full_name]}")
       @pull_request_info.each do |pull_request_info|
         pull_request = PullRequest.where(pr_github_ident: pull_request_info[:id]).first_or_initialize
 
         pull_request.update(pull_request_params(pull_request_info,repo_info))
+        pull_request_api_array << pull_request_info[:id]
       end
     end
+    GithubApi::Database_Updater.new.update_database_to_match_api(model_call: "pull_request", api_ids: pull_request_api_array, repo_ids: repo_id_array)
   end
 
   private
